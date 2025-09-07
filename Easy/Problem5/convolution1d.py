dbcb0d16007802a00fcc93197c8ee751a6c1bd62
A-A7med-i <an223602@gmail.com>
2025-09-07 23:36:32 +0300

    Added new problem

---
+++ Easy/Problem5/convolution1d.py
@@ -0,0 +1,15 @@
+import torch
+
+
+def solve(
+    input: torch.Tensor,
+    kernel: torch.Tensor,
+    output: torch.Tensor,
+    input_size: int,
+    kernel_size: int,
+):
+    windows = input.unfold(0, kernel_size, 1)
+
+    result = (windows * kernel).sum(dim=1)
+
+    output.copy_(result)
